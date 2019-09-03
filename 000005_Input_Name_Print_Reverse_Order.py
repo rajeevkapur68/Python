015d6117601679100d86ac069cb378989100c4c1
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 15:38:36 -0700

    Create 000005_Input_Name_Print_Reverse_Order.py

---
+++ 000005_Input_Name_Print_Reverse_Order.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 15:32:44 2019
+
+@author: rajee
+ Write a Python program which accepts the user's first and last name and 
+ print them in reverse order with a space between them
+"""
+
+firstName = input("Input First Name: ")
+lastName = input("Input Last Name: ")
+
+print(lastName + " " + firstName)
