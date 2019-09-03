7ec078d2224a85b773b8b87888acf72221bfca29
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 15:09:54 -0700

    Create Python_basic_Part_I_0002.py

---
+++ Python_basic_Part_I_0002.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 15:01:17 2019
+
+@author: rajee
+
+Write a Python program to get the Python version you are using.
+"""
+import sys
+print("Python Version: "+ str(sys.version))
+print("Python Version Info: " + str(sys.version_info))
