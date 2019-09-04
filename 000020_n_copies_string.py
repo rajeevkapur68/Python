faf63605036b03184b1cf3472ae40fea87aa5f1f
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-04 06:08:08 -0700

    copies given string n times
    Write a Python program to get a string which is n (non-negative integer)
    copies of a given string.

---
+++ 000020_n_copies_string.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 05:55:04 2019
+
+@author: rajee
+Write a Python program to get a string which is n (non-negative integer) 
+copies of a given string.
+"""
+
+myStr = print( (input("Input string: ") ) * \
+                abs(int(input("Copies : " ) )) \
+             )
