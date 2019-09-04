aeb55b064d7380a4fc7d59d7d35caaf78ce4bcb6
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-04 16:55:07 -0700

    area of triangle
    30. Write a Python program that will accept the base and height of a triangle
    and compute the area.

---
+++ 000030_traingle_area.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 16:44:24 2019
+
+@author: rajee
+30. Write a Python program that will accept the base and height of a triangle 
+and compute the area.
+A=(b/h)*2
+"""
+b=int(input("Base: "))
+h=int(input("height: "))
+print("Area: " + str((b*h)/2))
