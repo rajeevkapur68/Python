eb9540274403f8f1a58f975c943947db9e57137e
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 18:58:30 -0700

    print calendar of a given month and year.
    Write a Python program to print the calendar of a given month and year.

---
+++ 000012_print_calendar_year_month.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 18:47:01 2019
+
+@author: rajee
+Write a Python program to print the calendar of a given month and year.
+"""
+
+import calendar
+print(calendar.month(eval(input("Input year: ")),eval(input("Input Month: "))))
+
