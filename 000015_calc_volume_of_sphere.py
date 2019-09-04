80e30ef51c194b0460043a91ba01c8eaa955f8d9
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 19:47:26 -0700

    Calculate volume of a sphere
     Write a Python program to get the volume of a sphere with radius 6.

---
+++ 000015_calc_volume_of_sphere.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 19:40:23 2019
+
+@author: rajee
+ Write a Python program to get the volume of a sphere with radius 6.
+"""
+import math
+print("Volume of Sphere is : " + str(((4/3)*pi)*(int(input("Enter radius of sphere: "))**2)))
