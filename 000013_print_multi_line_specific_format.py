ada88a4a90e9164849a897c2c4372c958190a29c
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 19:03:58 -0700

    print multi line donot use escape
    Write a Python program to print the following 'here document'.
    
    Sample string:
    a string that you "don't" have to escape
    This
    is a ....... multi-line
    heredoc string --------> example

---
+++ 000013_print_multi_line_specific_format.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 19:00:52 2019
+
+@author: rajee
+Write a Python program to print the following 'here document'.
+
+Sample string:
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+"""
+
+print("""\ra string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example""")
