a3b6d684552af16d069059aa3569f51d58836f2c
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 17:55:43 -0700

    accept a filename, print extension
    Write a Python program to accept a filename from the user and
    print the extension of that.

--- 000007_print_filename_extension.py
+++ 000007_print_filename_extension.py
@@ -5,6 +5,7 @@ Created on Tue Sep  3 15:49:00 2019
 @author: rajee
 Write a Python program to accept a filename from the user and 
 print the extension of that.
+
 """
 filename = input("Input filename with extension: ")
 print("File extension is " + repr(filename.split('.')[-1]))
