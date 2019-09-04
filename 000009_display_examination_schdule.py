d7725194c9a1c9f82a3bf8cf32021ee9e46fdfb2
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 17:24:04 -0700

    extract the date from exam_st_date
     Write a Python program to
     display the examination schedule. (extract the date from exam_st_date).

---
+++ 000009_display_examination_schdule.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 17:12:40 2019
+
+@author: rajee
+ Write a Python program to 
+ display the examination schedule. (extract the date from exam_st_date).
+"""
+
+exam_st_date = (11, 12, 2014)
+print("The Examination will start from : %i/%i/%i" %(exam_st_date[0],exam_st_date[1],exam_st_date[2]))
