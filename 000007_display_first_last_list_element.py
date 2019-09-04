3e4e420ce5db6546c5fff6bf648c1bd4e411d266
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 17:11:35 -0700

    Create 000007_display_first_last_list_element.py

---
+++ 000007_display_first_last_list_element.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 17:04:50 2019
+
+@author: rajee
+Write a Python program to display the first and last colors 
+from the following list.
+"""
+myList = input("Input elements for list seperated by commas: ").split(',')
+print("First list element: " + str(myList[0]) + " Last List element: " + str(myList[-1]))
+
