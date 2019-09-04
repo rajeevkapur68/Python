a8113b62b8a6a5c6fc43baa03e2209561c22fa58
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-03 17:54:29 -0700

    accepts a sequence of comma-separated generate list, tuple
    Write a Python program which accepts a sequence of comma-separated numbers from user
    and generate a list and a tuple with those numbers.

--- 000006_accepts_number_create_list_tuple.py
+++ 000006_accepts_number_create_list_tuple.py
@@ -5,6 +5,7 @@ Created on Tue Sep  3 15:39:51 2019
 @author: rajee
 Write a Python program which accepts a sequence of comma-separated numbers from user
 and generate a list and a tuple with those numbers.
+
 """
 
 number_string = input("Input numbers separated by commas : ")
