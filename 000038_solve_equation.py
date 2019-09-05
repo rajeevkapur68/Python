eebe1da0a1c1673d9a965ec43ec556ef4db99c74
Rajeev Kapur <rajeevkapur2101@gmail.com>
2019-09-05 05:38:32 -0700

    solve (x + y) * (x + y).
    Write a Python program to solve (x + y) * (x + y).
    Test Data : x = 4, y = 3
    Expected Output : (4 + 3) ^ 2) = 49

---
+++ 000038_solve_equation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 05:32:26 2019
+
+@author: rajee
+Write a Python program to solve (x + y) * (x + y). 
+Test Data : x = 4, y = 3
+Expected Output : (4 + 3) ^ 2) = 49
+"""
+x,y=4,3
+print("(%i + %i)^%i = %i"%(x,y,2,(x+y)*(x+y)))
